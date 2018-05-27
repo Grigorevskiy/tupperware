169c9aa0daa3b5e99ca2f5cde9dfdd37167dd1d5
grigorevskiy <grigorevskiiy@gmail.com>
2018-05-27 22:10:22 +0300

    Added cart urls Added icons for cart wishlist Refactoring

--- cart/urls.py
+++ cart/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('', views.get_cart, name = "get_cart"),
+    path('add/<int:item_id>', views.add_to_cart, name = "add_to_cart"),
+    path('remove/<int:item_id>', views.remove_from_cart, name = "remove_from_cart"),
     ]
