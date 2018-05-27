eb439b31b398db6dd75742be64b7c412e0c4ebd2
grigorevskiy <grigorevskiiy@gmail.com>
2018-05-27 13:37:50 +0300

    Added Search backend Added at pages app view for FAQ, Delivery, About us page

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render
-
 from product.models import Category, Item
 
 
 def home(request):
-
     return render(request, 'home.html')
+
+# def about_us(request):
+#     return render(request, 'about_us.html')
+#
+#
+# def delivery(request):
+#     return render(request, 'delivery.html')
+#
+#
+# def faq(request):
+#     return render(request, 'faq.html')
+
+
+def search(request):
+    categories = Category.objects.all()
+    query_item_name = request.GET.get('q')
+    query_category_id = request.GET.get('category_id')
+    if query_category_id:
+        items = Item.objects.exclude(status=0).filter(category=query_category_id, title__icontains=query_item_name)
+        return render(request, 'search.html', {'items': items, 'categories': categories, 'query_item_name':query_item_name, 'query_category_id':query_category_id})
+    else:
+        items = Item.objects.exclude(status=0).filter(title__icontains=query_item_name)
+        return render(request, 'search.html', {'items': items, 'categories': categories, 'query_item_name':query_item_name})
 
 
