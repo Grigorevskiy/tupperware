f72dbf763d820d81f7647a54e378efc54a1f6102
Tetiana Bysaha <tanyabysaha@gmail.com>
2018-06-03 10:58:02 +0300

    1) Created item details page 2) Added hover to icons add to wishlist and add to card 4) Added link from a card to item details 5) Created similar block in item details

--- product/urls.py
+++ product/urls.py
@@ -5,5 +5,6 @@ from product import views
 urlpatterns = [
     path('<int:item_id>/comment/create', views.create_comment, name='create_comment'),
     path('<int:item_id>/comment/<int:comment_id>/delete', views.delete_comment, name='delete_comment'),
-    path('<int:item_id>/comment/<int:comment_id>/update', views.update_comment, name='update_comment')
+    path('<int:item_id>/comment/<int:comment_id>/update', views.update_comment, name='update_comment'),
+    path('<int:item_id>/details', views.details, name='item_details')
     ]
