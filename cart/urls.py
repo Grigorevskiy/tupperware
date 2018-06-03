c105f6115081da22b0ae26600561f7c7fb32933d
grigorevskiy <grigorevskiiy@gmail.com>
2018-06-03 11:53:30 +0300

    another approach (cart)

--- cart/urls.py
+++ cart/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.get_cart, name = "get_cart"),
     path('add/<int:item_id>', views.add_to_cart, name = "add_to_cart"),
     path('remove/<int:item_id>', views.remove_from_cart, name = "remove_from_cart"),
+    path('additional/<int:item_id>', views.additional_item, name = "additional_item"),
+
 ]
--- cart/views.py
+++ cart/views.py
@@ -2,7 +2,7 @@ from django.contrib import messages
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Cart
 from product.models import Item
-from . forms import CreateOrder, Additional_order_info
+from . forms import CreateOrder
 from order.models import Order, OrderInfo
 
 
@@ -19,13 +19,13 @@ def get_cart(request):
                 cart_total += i.price
 
         form = CreateOrder()
-        form_info = Additional_order_info()
-
+        # form_info = Additional_order_info()
 
         if request.method == "POST":
             order = Order.objects.create(user=request.user, total=cart_total,
                                          delivery_address=request.POST['delivery_address'],
                                          contact_phone=request.POST['contact_phone'])
+
 
             for item in cart.item.all():
                 order.items.add(item)
@@ -35,29 +35,33 @@ def get_cart(request):
 
             return redirect('/')
 
-        return render(request, 'cart/cart.html', {'cart': cart, 'cart_total': cart_total, 'form': form, 'form_info' : form_info})
+        return render(request, 'cart/cart.html', {'cart': cart, 'cart_total': cart_total, 'form': form})
     else:
         messages.info(request, ('Please login or register first!'))
         return redirect('/')
 
 
-def add_additional_item(request, item_id):
-    if request.user.is_authenticated:
-        if request.method == "GET":
-            cart, created = Cart.objects.get(user_id=request.user.id)
-            item = get_object_or_404(Item, id=item_id)
-            cart.item.add(item)
-            messages.success(request, ('You have added item to cart!'))
-    else:
-        messages.info(request, ('Please login or register first!'))
+def additional_item(request, item_id):
+    cart, created = Cart.objects.get_or_create(user_id=request.user.id)
+    form = CreateOrder()
 
-    return render(request, '')
+    s = OrderInfo.objects.get_or_create(item_id_id=item_id)
+
+    s.count_item += 1
 
 
-def remove_additional_item(request, item_id):
+    cart_total = 0
+    for i in cart.item.all():
+        if s.count_item > 1:
+            new_price = s.count_item * i.price
+        if i.sale_price:
+            cart_total += i.sale_price
+        else:
+            cart_total += i.price
 
 
-    return render(request, '')
+    return render(request, 'cart/cart.html',
+                  {'cart': cart, 'cart_total': cart_total, 'form': form, 'count': s.count_item})
 
 
 def add_to_cart(request, item_id):
